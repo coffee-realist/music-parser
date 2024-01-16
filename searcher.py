4432544e4b30768815688b055a4088025a1109b9
cr <cswd@yandex.ru>
2024-01-17 00:50:51 +0700

    Изменена область обнаружения текста песни, добавлены дополнительные переменные для удобства

--- searcher.py
+++ searcher.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 from bs4 import BeautifulSoup
 import re
 
-song_name = "applause"
+song_name = "blinding lights the weeknd"
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 url = f"https://genius.com/search?q={song_name}"
@@ -17,15 +16,14 @@ driver.get(models[0].get('href'))
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 text = []
-for element in soup.find_all('a', class_="ReferentFragmentdesktop__ClickTarget-sc-110r0d9-0 cehZkS"):
+for element in soup.find_all('div', class_="Lyrics__Container-sc-1ynbvzw-1 kUgSbL"):
     begin_index = 0
-    txt = element.span.text
+    txt = element.text
     for i in list(re.finditer(r'([^ \-(\[{\'A-ZА-Я]+[A-ZА-Я])', txt)):
-        text.append(txt[begin_index:(i.span()[1] - 1)])
-        begin_index = i.span()[1] - 1
+        end_index = i.span()[1] - 1
+        text.append(txt[begin_index:end_index])
+        begin_index = end_index
     end = txt[begin_index:]
     if end:
         text.append(end)
 print('\n'.join(text))
-
-
